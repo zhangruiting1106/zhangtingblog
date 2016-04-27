148485b24d60bfc405bc8c6a17a1b42b53f4d404
zhangruiting1106 <86109157@qq.com>
2016-04-27 13:57:27 +0800

    张博客

---
+++ blogbase.js
@@ -0,0 +1,11 @@
+//1.打开cmd --> npm install -g express-generator,安装express命令行工具，使用它初始化一个express项目
+//2.生成项目，-e表示ejs模板   express -e zhangtingblog
+//3.进入项目并安装依赖   cd zhangtingblog && npm install
+//4.设置环境变量并启动服务器   SET DEBUG=zhangtingblog:* & npm start
+//5.访问localhost:3000，看是否启动成功
+//6.在文件夹中新建.gitignore文件，忽略node_modules .idea lib
+//7.将建好的文件初始化为本地仓库  git init
+//8.将仓库放到历史区  git add -A  git commit -m"初始化博客仓库"
+//9.在gitHub上创建远程空仓库
+//10.将本地仓库推送到远程仓库 git remote add origin git push -u origin master
+
